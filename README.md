32d01dd09005b8addd7a27375fbe9bd08ffa8e98
Aitor <aitirga@gmail.com>
2024-01-10 23:09:50 +0100

    refactor: add feature to generate PDF

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pdf_summary
 This repo uses ChatGPT to summarize pdf files
+
+# Installation
+ Install `brew install Caskroom/cask/wkhtmltopdf`
+
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import openai
 import PyPDF2 as pp
 from dotenv import load_dotenv
 import os
+import pdfkit
+import markdown
 load_dotenv()
 
 text_to_num = {
@@ -10,6 +12,11 @@ text_to_num = {
     'mitjà': 500,
     'llarg': 1000,
 }
+
+# Add to session state
+if 'summary_done' not in st.session_state:
+    st.session_state.summary_done = False
+
 
 def read_file(file):
     viewer = pp.PdfReader(file)
@@ -25,29 +32,67 @@ def main():
     st.markdown("""
     
     """)
-    with st.container(border=True):
-        col1, col2 = st.columns(2, gap="medium")
-        with col1:
-            number = st.select_slider("Selecciona la llargada del resum", options=["curt", "mitjà", "llarg"], value="curt")
-        with col2:
-            lang = st.selectbox("Escull l'idioma", options=['Català', 'Español', 'English'], index=0)
-    file = st.file_uploader("Introdueix aquí el fitxer '.pdf' per resumir.", type=[".pdf"])
+    with st.form(key="resumidor"):
+        with st.container(border=True):
+            col1, col2 = st.columns(2, gap="medium")
+            with col1:
+                number = st.select_slider("Selecciona la llargada del resum", options=["curt", "mitjà", "llarg"], value="curt")
+            with col2:
+                lang = st.selectbox("Escull l'idioma", options=['Català', 'Español', 'English'], index=0)
+        file = st.file_uploader("Introdueix aquí el fitxer '.pdf' per resumir.", type=[".pdf"])
+        button = st.form_submit_button("Resumeix")
+        if button:
+            st.session_state['summary_done'] = False
     if file is not None:
         with st.spinner("Llegint..."):
             text = read_file(file)
-        with st.spinner("Resumint..."):
-            initial_message = {"role": "user", "content": f"""
-            Resumeix el text següent de manera que quedin ben clares les idees principals en un màxim de {text_to_num[number]} paraules. 
-            Escriu-ho tot en format markdown i utilitza les features de markdown, sobre tot bullet points i negretes. També els èmfasis de paraules ``.
-            Fes-ho en l'idioma {lang}.
-            
-            {text}
-            """}
-            openai_client = openai.OpenAI(api_key=os.environ["OPENAI_APIKEY"])
-            response = openai_client.chat.completions.create(model="gpt-4-1106-preview", messages=[initial_message])
-            print(response.choices[0].message.content)
-            st.markdown(response.choices[0].message.content)
+        if not st.session_state['summary_done']:
+            with st.spinner("Resumint..."):
+                initial_message = {"role": "user", "content": f"""
+                Resumeix el text següent de manera que quedin ben clares les idees principals en un màxim de {text_to_num[number]} paraules. 
+                Escriu-ho tot en format markdown i utilitza les features de markdown, sobre tot bullet points i negretes. També els èmfasis de paraules ``.
+                Fes-ho en l'idioma {lang}.
+                
+                {text}
+                """}
+                openai_client = openai.OpenAI(api_key=os.environ["OPENAI_APIKEY"])
+                response = openai_client.chat.completions.create(model="gpt-4-1106-preview", messages=[initial_message])
+                with open("output.md", "w") as file:
+                    file.write(response.choices[0].message.content)
+                st.markdown(response.choices[0].message.content)
+                st.session_state['summary_done'] = True
+            with st.spinner("Generant pdf per descarregar..."):
+                markdown_text = response.choices[0].message.content
 
+                # Convert Markdown to HTML
+                html_text = markdown.markdown(markdown_text)
+
+                # Create a temporary HTML file
+                with open("temp.html", "w") as file:
+                    file.write(html_text)
+
+                # Convert HTML to PDF
+                pdfkit.from_file("temp.html", "output.pdf")
+
+                # Remove the temporary HTML file
+                os.remove("temp.html")
+
+                # Provide the PDF for download
+                with open("output.pdf", "rb") as file:
+                    st.download_button(label="Download PDF",
+                                       data=file,
+                                       file_name="summary.pdf",
+                                       mime="application/pdf",
+                                         )
+        else:
+            with open("output.md", "r") as file:
+                st.markdown(file.read())
+            with open("output.pdf", "rb") as file:
+                st.download_button(label="Download PDF",
+                                   data=file,
+                                   file_name="summary.pdf",
+                                   mime="application/pdf",
+                                   )
 
 if __name__ == '__main__':
     main()
